bef1c6c0629276930bee5670914e44269e099dc9
heejoo <hjjunghj0427@gmail.com>
2026-09-16 14:39:18 +0900

    merge remote readme

---
+++ src/main/java/com/gildedrose/SulfurasItem.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+public class SulfurasItem extends GildedRoseItem {
+    public SulfurasItem(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void updateQuality(Item item) {
+        // 변경 없음
+    }
+}
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,27 +1,103 @@
 package com.gildedrose;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GildedRoseTest {
 
-	@Test
-	public void test() {
+	public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	public static final String NONAME = "noname";
+	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
 
-		Item[] items = new Item[]{
-				new Item("naname", 0, 0)
-		};
+	private void assertUpdateQuality(String name, int sellIn, int quality,
+									 int expectedSellIn, int expectedQuality) {
+		Item[] items = new Item[]{new Item(name, sellIn, quality)};
 		GildedRose gildedRose = new GildedRose(items);
 
 		gildedRose.updateQuality();
 
-		assertEquals(1, items.length);
-		assertEquals(-1, items[0].sellIn);
-		//assertEquals(0, items[0].quality);
+		assertEquals(expectedSellIn, items[0].sellIn, "sellIn 불일치");
+		assertEquals(expectedQuality, items[0].quality, "quality 불일치");
+	}
+
+	@Test
+	public void should_be_nothing_when_no_item() {
+		Item[] items = new Item[]{};
+		GildedRose gildedRose = new GildedRose(items);
+
+		gildedRose.updateQuality();
+
+		assertEquals(0, items.length);
+	}
 
 
+	@Test
+	public void normal_item_sellin_0_quality_0() {
+		assertUpdateQuality(NONAME, 0, 0, -1, 0);
+	}
+
+	@Test
+	public void normal_item_sellin_0_quality_1() {
+		assertUpdateQuality(NONAME, 0, 1, -1, 0);
+	}
+
+	@Test
+	public void normal_item_sellin_5_quality_10() {
+		assertUpdateQuality(NONAME, 5, 10, 4, 9);
+	}
+
+
+	@Test
+	public void sulfuras_sellin_0_quality_80() {
+		assertUpdateQuality(SULFURAS, 0, 80, 0, 80);
+	}
+
+	@Test
+	public void sulfuras_sellin_m2_quality_80() {
+		assertUpdateQuality(SULFURAS, -2, 80, -2, 80);
+	}
+
+	@Test
+	public void agedBrie_sellin_0_quality_0() {
+		assertUpdateQuality(AGED_BRIE, 0, 0, -1, 2);
+	}
+
+	@Test
+	public void agedBrie_sellin_5_quality_10() {
+		assertUpdateQuality(AGED_BRIE, 5, 10, 4, 11);
+	}
+
+	@Test
+	public void agedBrie_sellin_0_quality_50() {
+		assertUpdateQuality(AGED_BRIE, 0, 50, -1, 50);
+	}
+
+
+	@Test
+	public void backstagePass_sellin_0_quality_0() {
+		assertUpdateQuality(BACKSTAGE_PASS, 0, 0, -1, 0);
+	}
+
+	@Test
+	public void backstagePass_sellin_0_quality_49() {
+		assertUpdateQuality(BACKSTAGE_PASS, 0, 49, -1, 0);
+	}
+
+	@Test
+	public void backstagePass_sellin_12_quality_0() {
+		assertUpdateQuality(BACKSTAGE_PASS, 12, 0, 11, 1);
+	}
+
+	@Test
+	public void backstagePass_sellin_8_quality_10() {
+		assertUpdateQuality(BACKSTAGE_PASS, 8, 10, 7, 12);
+	}
+
+	@Test
+	public void backstagePass_sellin_3_quality_10() {
+		assertUpdateQuality(BACKSTAGE_PASS, 3, 10, 2, 13);
 	}
 
 }
